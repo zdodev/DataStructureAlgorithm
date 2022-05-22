511d1004f2942168f4761d5caa5cca6a1348c21d
zdodev <iver0945@gmail.com>
2022-05-22 22:43:36 +0900

    🧮: 백준 24262번 알고리즘 수업 - 알고리즘의 수행 시간 1

---
+++ Baekjoon/24262-알고리즘 수업 - 알고리즘의 수행 시간 1/24262-알고리즘 수업 - 알고리즘의 수행 시간 1/main.swift
@@ -0,0 +1,14 @@
+struct Solution {
+    private func readInteger() -> Int {
+        Int(readLine()!)!
+    }
+    
+    func solution() {
+        let _ = readInteger()
+        print(1)
+        print(0)
+    }
+}
+
+Solution().solution()
+
